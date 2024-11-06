b7556181660796b227c4e9ec85fb167889544df1
MayPyone <maypyone015@gmail.com>
2024-11-06 08:23:05 +0630

    Add login and create doctors endpoints

---
+++ backend/middlewares/authAdmin.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+
+//admin suthentication middleware
+
+const authAdmin = async (req,res,next) => {
+  const {atoken } = req.headers
+    try{
+      const {atoken } = req.headers
+      if(!atoken){
+        return res.json({success:false, message: 'Not Authorized Login again'})
+      }
+      const token_decode = jwt.verify(atoken,process.env.JWT_SECRET)
+       
+      if(token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){
+        return res.json({success:false, message: 'Not Authorized Login Againnn'})
+      }
+      next()
+    }catch(error){
+        res.json({success:false, message: 'invalid credential'})
+    }
+}
+
+export default authAdmin
