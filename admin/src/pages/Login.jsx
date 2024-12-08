1d23a273937fa6bafa385487b934a9027261ec45
MayPyone <maypyone015@gmail.com>
2024-12-08 12:41:10 +0630

    Create Doc pannel

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -3,23 +3,32 @@ import {assets} from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { DoctorContext } from '../context/DocorContext'
 
 function Login() {
   const [state, setState] = useState('Admin')
   const [email,setEmail] = useState('')
   const [password, setPassword] = useState('')
   const {setAToken, backendUrl} = useContext(AdminContext)
+  const {setDtoken} = useContext(DoctorContext)
   
   const onSubmitHandler = async (event) => {
     event.preventDefault()
     try{
       if(state === "Admin"){
         const { data } = await axios.post(backendUrl + '/api/admin/login', { email, password });
-     
-        console.log(data)
         if(data.success){
           localStorage.setItem('aToken',data.token)
           setAToken(data.token)
+        }else{
+          toast.error(data.message)
+        }
+      }else{
+        const { data } = await axios.post(backendUrl + '/api/doctor/login',{ email, password });
+        if(data.success){
+          localStorage.setItem('dToken',data.token)
+          setDtoken(data.token)
+          console.log(data.token)
         }else{
           toast.error(data.message)
         }
